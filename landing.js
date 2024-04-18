c2c72ef7f0331a0d5159f14cec41682e4266494e
yogeshwariomkumar <160750568+yogeshwariomkumar@users.noreply.github.com>
2024-04-18 18:56:55 +0530

    Update landing.js

--- landing.js
+++ landing.js
@@ -1,4 +1,4 @@
-// Smooth scrolling for navigation links
+// Smooth scrolling for navigation link
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
